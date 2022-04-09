0f886082622e39555b9146928f0fbfb8e6518546
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2022-04-10 01:21:58 +0530

    Format document with prettier

--- server.c
+++ server.c
@@ -27,25 +27,26 @@ typedef struct
     char status_code[MSG_VAL_LEN];
 } server_msg_t;
 
-
 int MIN_COURSES = 10;
 int MAX_COURSES = 15;
 int MIN_TEACHERS = 5;
 int MAX_TEACHERS = 10;
 
-struct Teacher {
+struct Teacher
+{
     char name[TEACHER_NAME_LEN];
 } teachers[10];
 
-struct Course {
+struct Course
+{
     char name[COURSE_NAME_LEN];
     char teacher[TEACHER_NAME_LEN];
 } courses[15];
 
 sem_t bin_sem;
 
-
-void init_config(int configurable) {
+void init_config(int configurable)
+{
     /*
     The initial configuration of the Edu Server with respect to the minimum and maximum values are configurable.
     MIN_COURSES 10, MAX_COURSES 15, MIN_TEACHERS 5, MAX_TEACHERS 10
@@ -55,39 +56,47 @@ void init_config(int configurable) {
     */
 
     int min_courses, max_courses, min_teachers, max_teachers;
-    
+
     printf("Please set these initial configurations of the server:\n");
 
-    if(configurable) {
-    printf("MIN COURSES (min 10): ");
-    scanf("%d", &min_courses);
-    printf("MAX COURSES (max 15): ");
-    scanf("%d", &max_courses);
-    printf("MIN TEACHERS (min 5): ");
-    scanf("%d", &min_teachers);
-    printf("MAX COURSES (max 10): ");
-    scanf(" %d%*c", &max_teachers); // Space is required before %d : https://stackoverflow.com/questions/26085237/simple-c-code-not-working
+    if (configurable)
+    {
+        printf("MIN COURSES (min 10): ");
+        scanf("%d", &min_courses);
+        printf("MAX COURSES (max 15): ");
+        scanf("%d", &max_courses);
+        printf("MIN TEACHERS (min 5): ");
+        scanf("%d", &min_teachers);
+        printf("MAX COURSES (max 10): ");
+        scanf(" %d%*c", &max_teachers); // Space is required before %d : https://stackoverflow.com/questions/26085237/simple-c-code-not-working
 
-    if(min_courses <= max_courses) {
-        if(min_courses >= MIN_COURSES && min_courses <= MAX_COURSES) {
-            MIN_COURSES = min_courses;
+        if (min_courses <= max_courses)
+        {
+            if (min_courses >= MIN_COURSES && min_courses <= MAX_COURSES)
+            {
+                MIN_COURSES = min_courses;
+            }
+            if (max_courses >= MIN_COURSES && max_courses <= MAX_COURSES)
+            {
+                MAX_COURSES = max_courses;
+            }
         }
-        if(max_courses >= MIN_COURSES && max_courses <= MAX_COURSES) {
-            MAX_COURSES = max_courses;
+
+        if (min_teachers <= max_teachers)
+        {
+            if (min_teachers >= MIN_TEACHERS && min_teachers <= MAX_TEACHERS)
+            {
+                MIN_TEACHERS = min_teachers;
+            }
+            if (max_teachers >= MIN_TEACHERS && max_teachers <= MAX_TEACHERS)
+            {
+                MAX_TEACHERS = max_teachers;
+            }
         }
     }
 
-    if(min_teachers <= max_teachers) {
-        if(min_teachers >= MIN_TEACHERS && min_teachers <= MAX_TEACHERS) {
-            MIN_TEACHERS = min_teachers;
-        }
-        if(max_teachers >= MIN_TEACHERS && max_teachers <= MAX_TEACHERS) {
-            MAX_TEACHERS = max_teachers;
-        }
-    }
-    }
-
-    for(int i = 0; i < MAX_COURSES; i++) {
+    for (int i = 0; i < MAX_COURSES; i++)
+    {
         char name[COURSE_NAME_LEN] = "NULL";
         char teacher[TEACHER_NAME_LEN] = "NULL";
         struct Course c;
@@ -96,7 +105,8 @@ void init_config(int configurable) {
         courses[i] = c;
     }
 
-    for(int i = 0; i < MAX_TEACHERS; i++) {
+    for (int i = 0; i < MAX_TEACHERS; i++)
+    {
         char name[TEACHER_NAME_LEN] = "NULL";
         struct Teacher t;
         strncpy(t.name, name, sizeof(t.name));
@@ -107,35 +117,43 @@ void init_config(int configurable) {
     return;
 }
 
-int add_course(char* name) {
+int add_course(char *name)
+{
     // Checking if the course already exists
-    for(int i = 0; i < MAX_COURSES; i++) {
+    for (int i = 0; i < MAX_COURSES; i++)
+    {
         struct Course c = courses[i];
-        if(strcmp(name, c.name) == 0) {
+        if (strcmp(name, c.name) == 0)
+        {
             return COURSE_DUPLICATE; // ERROR: Course with given name already exists
         }
     }
 
     // Adding course to the first place where name is NULL
-    for(int i = 0; i < MAX_COURSES; i++) {
+    for (int i = 0; i < MAX_COURSES; i++)
+    {
         struct Course *c = malloc(sizeof(struct Course));
         c = &courses[i];
-        if(strcmp("NULL", c->name) == 0) {
+        if (strcmp("NULL", c->name) == 0)
+        {
             strcpy(c->name, name);
 
             // Assign teacher randomly from current teachers
             struct Teacher available_teachers[10];
             int current_teacher = 0;
-            for(int i = 0; i < MAX_TEACHERS; i++) {
+            for (int i = 0; i < MAX_TEACHERS; i++)
+            {
                 struct Teacher *t = malloc(sizeof(struct Teacher));
                 t = &teachers[i];
-                if(strcmp("NULL", t->name) != 0) {
+                if (strcmp("NULL", t->name) != 0)
+                {
                     available_teachers[current_teacher] = *t;
                     current_teacher++;
                 }
             }
 
-            if(current_teacher != 0) {
+            if (current_teacher != 0)
+            {
                 int r = rand() % current_teacher;
                 struct Teacher t = available_teachers[r];
                 strcpy(c->teacher, t.name);
@@ -148,36 +166,43 @@ int add_course(char* name) {
     return MAX_COURSE_FULL; // ERROR: Maximum courses reached
 }
 
-int delete_course(char* name) {
+int delete_course(char *name)
+{
     // Checking if the course already exists or not
-    for(int i = 0; i < MAX_COURSES; i++) {
+    for (int i = 0; i < MAX_COURSES; i++)
+    {
         struct Course *c = malloc(sizeof(struct Course));
         c = &courses[i];
-        if(strcmp(name, c->name) == 0) {
+        if (strcmp(name, c->name) == 0)
+        {
             strcpy(c->name, "NULL");
             strcpy(c->teacher, "NULL");
-            return SUCCESS; 
+            return SUCCESS;
         }
     }
 
     return COURSE_NOT_EXISTS; // ERROR: Course does not exist so can't be removed
 }
 
-
-int add_teacher(char* name) {
+int add_teacher(char *name)
+{
     // Checking if the teacher already exists
-    for(int i = 0; i < MAX_TEACHERS; i++) {
+    for (int i = 0; i < MAX_TEACHERS; i++)
+    {
         struct Teacher t = teachers[i];
-        if(strcmp(name, t.name) == 0) {
+        if (strcmp(name, t.name) == 0)
+        {
             return TEACHER_DUPLICATE; // ERROR: Teacher with given name already exists
         }
     }
 
     // Adding teacher to the first place where name is NULL
-    for(int i = 0; i < MAX_TEACHERS; i++) {
+    for (int i = 0; i < MAX_TEACHERS; i++)
+    {
         struct Teacher *t = malloc(sizeof(struct Teacher));
         t = &teachers[i];
-        if(strcmp("NULL", t->name) == 0) {
+        if (strcmp("NULL", t->name) == 0)
+        {
             strcpy(t->name, name);
             return SUCCESS;
         }
@@ -186,44 +211,54 @@ int add_teacher(char* name) {
     return MAX_TEACHER_FULL; // ERROR: Maximum teachers reached
 }
 
-int delete_teacher(char* name) {
+int delete_teacher(char *name)
+{
     // Checking if the teacher already exists or not
-    for(int i = 0; i < MAX_TEACHERS; i++) {
+    for (int i = 0; i < MAX_TEACHERS; i++)
+    {
         struct Teacher *t = malloc(sizeof(struct Teacher));
         t = &teachers[i];
-        if(strcmp(name, t->name) == 0) {
+        if (strcmp(name, t->name) == 0)
+        {
 
-            for(int i = 0; i < MAX_COURSES; i++) {
+            for (int i = 0; i < MAX_COURSES; i++)
+            {
                 struct Course *c = malloc(sizeof(struct Course));
                 c = &courses[i];
-                if(strcmp(t->name, c->name) == 0) {
+                if (strcmp(t->name, c->name) == 0)
+                {
                     strcpy(c->teacher, "NULL");
                 }
             }
 
             strcpy(t->name, "NULL");
-            return SUCCESS; 
+            return SUCCESS;
         }
     }
 
     return TEACHER_NOT_EXISTS; // ERROR: Maximum teachers reached
 }
 
-void print_report(FILE *fptr) {
+void print_report(FILE *fptr)
+{
     fprintf(fptr, "\n------------REPORT STARTED----------------\n");
 
     fprintf(fptr, "COURSES: \n");
-    for(int i = 0; i < MAX_COURSES; i++) {
+    for (int i = 0; i < MAX_COURSES; i++)
+    {
         struct Course c = courses[i];
-        if(strcmp("NULL", c.name) != 0) {
+        if (strcmp("NULL", c.name) != 0)
+        {
             fprintf(fptr, "Name: %s Teacher: %s\n", c.name, c.teacher);
         }
     }
 
     fprintf(fptr, "TEACHERS: \n");
-    for(int i = 0; i < MAX_TEACHERS; i++) {
+    for (int i = 0; i < MAX_TEACHERS; i++)
+    {
         struct Teacher t = teachers[i];
-        if(strcmp("NULL", t.name) != 0) {
+        if (strcmp("NULL", t.name) != 0)
+        {
             fprintf(fptr, "Name: %s\n", t.name);
         }
     }
@@ -232,9 +267,11 @@ void print_report(FILE *fptr) {
     return;
 }
 
-void* generate_report() {
-    while(1) {
-        sem_wait(&bin_sem);  // Thread gets blocked if bin_sem is not free
+void *generate_report()
+{
+    while (1)
+    {
+        sem_wait(&bin_sem); // Thread gets blocked if bin_sem is not free
         print_report(stdout);
         sem_post(&bin_sem);
         sleep(10);
@@ -244,17 +281,15 @@ void* generate_report() {
 /* Signal Handler for SIGINT */
 void sigintHandler(int sig_num)
 {
-    /* Reset handler to catch SIGINT next time.
-       Refer http://en.cppreference.com/w/c/program/signal */
     signal(sig_num, SIG_IGN);
     FILE *fptr;
 
-    fptr = fopen("server.txt","w");
+    fptr = fopen("server.txt", "w");
 
-    if(fptr == NULL)
+    if (fptr == NULL)
     {
         printf("File opening error!");
-        exit(1);             
+        exit(1);
     }
 
     print_report(fptr);
@@ -262,18 +297,17 @@ void sigintHandler(int sig_num)
     exit(0);
 }
 
-
 int main(int argc, char **argv)
 {
     signal(SIGINT, sigintHandler);
 
-    srand(time(NULL));   // Random number initialized, should only be called once.
+    srand(time(NULL)); // Random number initialized, should only be called once.
 
     mqd_t qd_srv, qd_client; // Server and Client Msg queue descriptors
 
     printf("Welcome to the Education Server!!!\n");
     init_config(0); // Initial server configuration
-    
+
     struct mq_attr attr;
 
     attr.mq_flags = 0;
@@ -290,17 +324,19 @@ int main(int argc, char **argv)
 
     int res_sem, res;
     pthread_t thread;
-    
+
     res_sem = sem_init(&bin_sem, 0, 1);
-    if(res_sem != 0) {
+    if (res_sem != 0)
+    {
         printf("Semaphore creation failure: %d\n", res_sem);
         exit(1);
     }
 
-    if( (res = pthread_create( &thread, NULL, &generate_report, "Thread")) )  {
-      printf("Thread creation failed: %d\n", res);
-      exit(1);
-   }
+    if ((res = pthread_create(&thread, NULL, &generate_report, NULL)))
+    {
+        printf("Thread creation failed: %d\n", res);
+        exit(1);
+    }
 
     client_msg_t in_msg;
     int status;
@@ -324,16 +360,20 @@ int main(int argc, char **argv)
         printf("Client msg q name = %s\n", in_msg.client_q);
         printf("Query type = %s Details = %s.\n", client_query_type, client_query_details);
 
-        if(strcmp(client_query_type, ADD_COURSE) == 0) {
+        if (strcmp(client_query_type, ADD_COURSE) == 0)
+        {
             status = add_course(client_query_details);
         }
-        else if(strcmp(client_query_type, DELETE_COURSE) == 0) {
+        else if (strcmp(client_query_type, DELETE_COURSE) == 0)
+        {
             status = delete_course(client_query_details);
         }
-        else if(strcmp(client_query_type, ADD_TEACHER) == 0) {
+        else if (strcmp(client_query_type, ADD_TEACHER) == 0)
+        {
             status = add_teacher(client_query_details);
         }
-        else if(strcmp(client_query_type, DELETE_TEACHER) == 0) {
+        else if (strcmp(client_query_type, DELETE_TEACHER) == 0)
+        {
             status = delete_teacher(client_query_details);
         }
 
